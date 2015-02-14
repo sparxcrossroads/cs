e11bf9ba42eed6977dd641c5f36ced5ea52d9404
sparxcrossroads <kisshotblade@sina.com>
2015-02-14 19:58:53 +0800

    first commit

---
+++ Assets/Finished/Script/cubemoving.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubemoving : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public int movedSpeed=10;
+	public int rotation=20;
+
+	public bool isUP=false;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W)) 
+		{
+			transform.Translate (Vector3.forward * Time.deltaTime * movedSpeed);				
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate (Vector3.back * Time.deltaTime * movedSpeed);				
+		}
+	    if(Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.down*Time.deltaTime*rotation,Space.World);
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.up*Time.deltaTime*rotation,Space.World);
+		}
+		if (isUP)
+		{
+			transform.Translate(Vector3.up*Time.deltaTime*movedSpeed*movedSpeed);
+		}
+	}
+}
